7a207f96dc162727f119179debd4646a4cddbf88
Uhycore <arilmubin0@gmail.com>
2025-09-08 08:34:52 +0700

    fix asc with type

--- app/Http/Controllers/PropertiesControllerAsUser.php
+++ app/Http/Controllers/PropertiesControllerAsUser.php
@@ -9,7 +9,11 @@ class PropertiesControllerAsUser extends Controller
 {
     public function index()
     {
-        $properties = Properties::orderBy('name', 'asc')->get();
+        $properties = Properties::query()
+            ->orderByRaw("FIELD(type, 'aula','kelas','asrama','paviliun')")
+            ->orderBy('name', 'asc')
+            ->get();
+
 
         return view('user.properties_as_user', [
             'properties' => $properties
